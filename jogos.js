c124b1d1c9d410c971babd5e0a5d24bed9589bb5
Luanhts <luanh3nrique08@gmail.com>
2024-07-24 19:14:10 -0300

    Função de criar

---
+++ jogos.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+
+const jogos = []
+
+const criar = () => {
+    const nome = prompt("Nome do jogo: ")
+    const ano_lancamento = prompt("Ano de lançamento: ")
+    const duracao = prompt("Duração média de GamePlay: ")
+    const preco = prompt("Preço: ")
+    const estudio = prompt("Qual o estudio do jogo: ")
+    const sequencia = prompt("Qual a sequencia do jogo: ")
+
+    if(nome != "" && ano_lancamento >= 1962 && ano_lancamento <= 2024 && duracao > 0 && preco == 0 && estudio != "" && ((sequencia > 0 && sequencia < jogos.length) || jogos.length == 0)
+    ) {
+        jogos.push({
+            nome, ano_lancamento, duracao, preco, estudio, sequencia
+        })
+        console.log("Jogo Cadastrado: ")
+} else{
+    console.log("Dados inválidos: ")
+}
+}
+
+
+
